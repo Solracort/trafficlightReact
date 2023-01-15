095101501fcbad8ba9895cff4c7bce0f68e14448
Solracort <solracort@gmail.com>
2023-01-15 19:26:49 +0000

    Traffic Light REACT manejando event

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+//Semaforo hecho con React no uso la funcion cambiar estado, ni hago programacion declarativa, es mas, esto es lo mas imperativo que puede haber 
 
 //create your first component
 const Home = () => {
+
+	const [activo, setActivo] = useState("opacity-25");
+	
+	const colorRojo = "rojo btn btn-danger ";
+	const colorAmbar = "ambar btn btn-warning ";
+	const colorVerde = "verde btn btn-success ";
+	const colorLight =[colorRojo, colorAmbar, colorVerde];
+	const changeColor = (e) =>{
+		let valor = e.target.className;
+		console.log(valor);
+		if (valor === "rojo btn btn-danger opacity-25"){
+			e.target.className="rojo btn btn-danger opacity-100";
+		}else if (valor === "rojo btn btn-danger opacity-100"){
+			e.target.className="rojo btn btn-danger opacity-25";
+		}else if (valor === "ambar btn btn-warning opacity-25"){
+			e.target.className="ambar btn btn-warning opacity-100"
+		}else if (valor === "ambar btn btn-warning opacity-100"){
+			e.target.className="ambar btn btn-warning opacity-25"
+		}else if (valor === "verde btn btn-success opacity-25"){
+			e.target.className="verde btn btn-success opacity-100"
+		}else if (valor === "verde btn btn-success opacity-100"){
+			e.target.className="verde btn btn-success opacity-25"
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="semaforo ">
+			<div className= { colorLight[0] + activo} onClick={changeColor}>9</div>
+			<div className= { colorLight[1] + activo} onClick={changeColor}>9</div>
+			<div className= { colorLight[2] + activo} onClick={changeColor}>9</div>
 		</div>
 	);
 };
+
+
+
+
+
+
 
 export default Home;
